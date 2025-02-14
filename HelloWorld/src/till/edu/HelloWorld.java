f39d314f8c4048d1775bfb6bc3f7dacaa66eec00
Nguyễn Văn Tâm <tam.nv.61cntt@ntu.edu.vn>
2025-02-15 00:36:06 +0700

    Ví dụ Hello World

---
+++ HelloWorld/src/till/edu/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package till.edu;
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!! Tôi là Nguyễn Văn Tâm");
+	}
+
+}
